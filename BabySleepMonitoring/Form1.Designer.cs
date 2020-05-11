38234728ad1fbf1819df52ab30b05e0800dd5e86
Carlotta <Carlotta@DESKTOP-0BVGLP7>
2020-05-11 13:24:39 +0200

    User Control -> Bettrandmakierung

--- BabySleepMonitoring/Form1.Designer.cs
+++ BabySleepMonitoring/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ButtonOrdnerWählen = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ButtonOrdnerWählen
+            // 
+            this.ButtonOrdnerWählen.Location = new System.Drawing.Point(12, 12);
+            this.ButtonOrdnerWählen.Name = "ButtonOrdnerWählen";
+            this.ButtonOrdnerWählen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOrdnerWählen.TabIndex = 0;
+            this.ButtonOrdnerWählen.Text = "Ordner";
+            this.ButtonOrdnerWählen.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 553);
+            this.Controls.Add(this.ButtonOrdnerWählen);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonOrdnerWählen;
     }
 }
 
