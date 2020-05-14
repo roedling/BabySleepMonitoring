78d3666dc1770db4a7f8efc0406117b1725c1a2f
Carlotta <Carlotta@DESKTOP-0BVGLP7>
2020-05-14 09:32:59 +0200

    mouse position calculation

--- BabySleepMonitoring/Bettrandmakierung.cs
+++ BabySleepMonitoring/Bettrandmakierung.cs
@@ -17,10 +17,16 @@ namespace BabySleepMonitoring
             InitializeComponent();
         }
 
-
-        List<Point> eckpunkte = new List<Point>(3);
+        private Bitmap cloneImage = null;
+        private List<Point> eckpunkte = new List<Point>(3);
         private Bitmap image = null;
         int Checked = 0;
+        private Point m_mouseStart = new Point(0, 0);
+        private Point m_mouseCorrect = new Point();
+        private double xBox = new double();
+        private double yBox = new double();
+        private double xImage = new double();
+        private double yImage = new double();
 
 
         private string path;
@@ -31,7 +37,7 @@ namespace BabySleepMonitoring
                 path = value;
                 image = new Bitmap(path);
                 PictureBoxMakierung.Image = image;
-                if(PictureBoxMakierung.Image != null)
+                if (PictureBoxMakierung.Image != null)
                 {
                     textBox1.Visible = true;
                 }
@@ -50,62 +56,55 @@ namespace BabySleepMonitoring
 
         private void PictureBoxMakierung_MouseClick(object sender, MouseEventArgs e)
         {
-            if (Checked == 1)
+            if (Checked == 1 && image != null)
             {
-                int x = e.X;
-                int y = e.Y;
+                m_mouseStart = new Point(e.X, e.Y);
 
                 //Umberechnung Maus Position
-                int imageY = image.Height;
-                int imageX = image.Width;
-                int BoxY = PictureBoxMakierung.Height;
-                int BoxX = PictureBoxMakierung.Width;
-                double BoxVer = (double)BoxX / (double)BoxY;
-                double imageVer = (double)imageX / (double)imageY;
+                yImage = image.Height;
+                xImage = image.Width;
+                yBox = PictureBoxMakierung.Size.Height;
+                xBox = PictureBoxMakierung.Size.Width;
 
-                if (BoxVer < imageVer)  //oben unten 
+                double referenz = xImage / xImage;
+                double vergleich = xBox / yBox;
+
+                if (referenz < vergleich)
                 {
-                    int yWert = 0;
-                    int xWert = 0;
-                    var Punkt = new Point(xWert, yWert);
-                    eckpunkte.Add(Punkt);
+                    BildKoordinatenX();
+                    BildKoordinatenY();
                 }
-                else if (BoxVer > imageVer) //rechts links
+                else if (referenz > vergleich)
                 {
-
-                    int yWert = 0;
-                    int xWert = 0;
-                    var Punkt = new Point(xWert, yWert);
-                    eckpunkte.Add(Punkt);
+                    BildKoordinatenOX();
+                    BildKoordinatenOY();
                 }
                 else
                 {
-                    var Punkt = new Point(x, y);
-                    eckpunkte.Add(Punkt);
+                    m_mouseCorrect = new Point((int)xImage, (int)yImage);
                 }
 
+                eckpunkte.Add(m_mouseCorrect);
 
-                
-
-                Bitmap ImageClone = new Bitmap(image);
+                cloneImage = (Bitmap)image.Clone();
 
                 if (eckpunkte.Count == 2)
                 {
                     Graphics g = this.CreateGraphics();
-                    g = Graphics.FromImage(ImageClone);
+                    g = Graphics.FromImage(cloneImage);
                     Pen p = new Pen(Color.Red, 10);
                     Point point2 = new Point(eckpunkte[1].X, eckpunkte[0].Y);
                     g.DrawLine(p, eckpunkte[0], point2);
-              
-                    PictureBoxMakierung.Image = ImageClone;
+
+                    PictureBoxMakierung.Image = cloneImage;
 
                     PictureBoxMakierung.Refresh();
                 }
                 if (eckpunkte.Count == 3)
                 {
-                    
+
                     Graphics g = this.CreateGraphics();
-                    g = Graphics.FromImage(ImageClone);
+                    g = Graphics.FromImage(cloneImage);
                     Pen p = new Pen(Color.Red, 10);
                     Point point2 = new Point(eckpunkte[1].X, eckpunkte[0].Y);
                     Point point3 = new Point(eckpunkte[1].X, eckpunkte[2].Y);
@@ -115,7 +114,7 @@ namespace BabySleepMonitoring
                     g.DrawLine(p, point3, punkt4);
                     g.DrawLine(p, punkt4, eckpunkte[0]);
 
-                    PictureBoxMakierung.Image = ImageClone;
+                    PictureBoxMakierung.Image = cloneImage;
 
                     PictureBoxMakierung.Refresh();
                 }
@@ -125,9 +124,77 @@ namespace BabySleepMonitoring
         private void ButtonNeuMakierung_Click(object sender, EventArgs e)
         {
             eckpunkte.Clear();
+            Checked = 0;
             PictureBoxMakierung.Image = null;
             PictureBoxMakierung.Image = image;
 
+        }
+
+        private void BildKoordinatenY()
+        {
+            if (PictureBoxMakierung.Image != null)
+            {
+                double startProzenY = (100 * (double)m_mouseStart.Y) / yBox;
+                m_mouseCorrect.Y = (int)((yImage * startProzenY) / 100);
+            }
+
+        }
+        private void BildKoordinatenX()
+        {
+            if (PictureBoxMakierung.Image != null)
+            {
+
+                double yFaktor = yBox / yImage;
+
+                double ih = yFaktor * yImage;
+                double iw = yFaktor * xImage;
+
+                double diff = (xBox - iw) / 2;
+
+                    double mausStart = (m_mouseStart.X - diff);
+                    double startImageProzent = (100 * mausStart) / iw;
+                    m_mouseCorrect.X = (int)((xImage * startImageProzent) / 100);
+            }
+        }
+        private void BildKoordianten()
+        {
+            double referenz = xImage / xImage;
+            double vergleich = xBox / yBox;
+
+            if (referenz < vergleich)
+            {
+                BildKoordinatenX();
+                BildKoordinatenY();
+            }
+            if (referenz > vergleich)
+            {
+                BildKoordinatenOX();
+                BildKoordinatenOY();
+            }
+        }
+        private void BildKoordinatenOX()
+        {
+            if (PictureBoxMakierung.Image != null)
+            {
+                double startProzenY = (100 * (double)m_mouseStart.Y) / xBox;
+                m_mouseCorrect.X = (int)((xImage * startProzenY) / 100);
+            }
+        }
+        private void BildKoordinatenOY()
+        {
+            if (PictureBoxMakierung.Image != null)
+            {
+                double yFaktor = yBox / yImage;
+
+                double ih = yFaktor * yImage;
+                double iw = yFaktor * xImage;
+
+                double diff = (yBox - iw) / 2;
+
+                    double mausStart = (m_mouseStart.Y - diff);
+                    double startImageProzent = (100 * mausStart) / ih;
+                    m_mouseCorrect.Y = (int)((yImage * startImageProzent) / 100);
+            }
         }
     }
 }
