088163d852cd413c6c2092a23f74abe87ca881c2
Carlotta <Carlotta@DESKTOP-0BVGLP7>
2020-05-19 20:56:03 +0200

    comments

--- BabySleepMonitoring/Form1.cs
+++ BabySleepMonitoring/Form1.cs
@@ -13,7 +13,7 @@ namespace BabySleepMonitoring
 {
     public partial class Form1 : Form
     {
-
+        // Variablen
         private Bettrandmakierung m_Bettrandmakierung = null;
         private Ueberpruefen m_Ueberpruefen = null;
         
@@ -23,37 +23,36 @@ namespace BabySleepMonitoring
             InitializeComponent();
         }
 
-        
-        //Versuch1
-
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Initalisierung des UserControl "Bettrandmakierung"
+            m_Bettrandmakierung = new Bettrandmakierung(); //Erstellung
+            SplitContainer2.Panel1.Controls.Add(m_Bettrandmakierung); //Einfügen des UserControls an der richtigen Stelle
+            m_Bettrandmakierung.Dock = DockStyle.Fill; //UserControl soll (in dem Fall SplitContainer2.Panel1) komplett ausfüllen
+            m_Bettrandmakierung.Visible = false; //Anfangssichtbarkeit auf "undurchsichtig"
 
-            m_Bettrandmakierung = new Bettrandmakierung();
-            SplitContainer2.Panel1.Controls.Add(m_Bettrandmakierung);
-            m_Bettrandmakierung.Dock = DockStyle.Fill;
-            m_Bettrandmakierung.Visible = false;
-
+            //Initalisierung des UserControl "Ueberpruefen"
             m_Ueberpruefen = new Ueberpruefen();
             SplitContainer2.Panel2.Controls.Add(m_Ueberpruefen);
             m_Ueberpruefen.Dock = DockStyle.Fill;
             m_Ueberpruefen.Visible = false;
 
+            //Initalisierung der Funktion nav_Text, welche aus dem UserControl "Ueberpruefen" aufgerufen wird
             m_Bettrandmakierung.Test += new TestFunction(nav_Text);
         }
 
-        private void nav_Text()
+        private void nav_Text() //Funktion welche aus dem UserControl "Ueberpruefen" aufgerufen wird
         {
-            m_Ueberpruefen.Visible = true;
+            m_Ueberpruefen.Visible = true; //Sichtbarkeit des UserControls Ueberpruefen auf "sichtbar"
         }
 
-        private void ButtonOrdnerWählen_Click(object sender, EventArgs e)
+        private void ButtonOrdnerWählen_Click(object sender, EventArgs e) //Funktion welche beim Drücken des Buttons ausgeführt wird
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Wenn die Wahl der Datei erfolgt und korrekt ist dann ->
             {
-                m_Bettrandmakierung.Visible = true;
-                m_Bettrandmakierung.fillList_path = openFileDialog1.FileName;
-                m_Ueberpruefen.ueberpruefen_path = openFileDialog1.FileName;
+                m_Bettrandmakierung.Visible = true; //Sichtbarkeit des UserControls Bettrandmakierung auf "sichtbar"
+                m_Bettrandmakierung.fillList_path = openFileDialog1.FileName; //Übergabe des Pfads an Bettrandmakierung
+                m_Ueberpruefen.ueberpruefen_path = openFileDialog1.FileName; //Übergabe des Pfads an Ueberpruefen
 
             }
         }
