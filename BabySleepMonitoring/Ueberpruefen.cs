b8b746fef098e9f2a1a2dda86e50cfa27a2299a1
Kathrin Beyrle <kathrin@beyrle.de>
2020-05-20 22:55:27 +0200

    Comments "Ueberpruefen"

--- BabySleepMonitoring/Ueberpruefen.cs
+++ BabySleepMonitoring/Ueberpruefen.cs
@@ -18,13 +18,14 @@ namespace BabySleepMonitoring
             InitializeComponent();
         }
 
+        //Variablen
         private string folder;
         private Bitmap currentPic = null;
         private int i = 0;
         private Timer timer;
         private string[] files;
         private string path;
-        public string ueberpruefen_path
+        public string ueberpruefen_path //Funktion, die Pfad von Form1 bekommt und daraus erste Bildinstanz initialisiert
         {
             set
             {
@@ -33,38 +34,38 @@ namespace BabySleepMonitoring
             }
         }
 
-        private void ButtonStart_Click(object sender, EventArgs e)
+        private void ButtonStart_Click(object sender, EventArgs e) // Funktion, die bei clicken des Startbuttons ausgeführt wird:
         {
-            if(path != null)
+            if(path != null) //wenn Pfad vorhandne ist/Pfad übergeben wurde:
             {
-                folder = Path.GetDirectoryName(path);
-                files = Directory.GetFiles(folder);
+                folder = Path.GetDirectoryName(path); // werden Ornerinformationen und dazugehörende
+                files = Directory.GetFiles(folder); // Fileinformationen übergeben
 
-                timer = new Timer();
-                timer.Tick += new EventHandler(check);
-                timer.Interval = 2000; // in miliseconds
-                timer.Start();
+                timer = new Timer(); // Erstellung timer
+                timer.Tick += new EventHandler(check); //Initialisierung der Funktion check
+                timer.Interval = 2000; // Intervall auf 2000 miliseconds festgelegt
+                timer.Start(); //start
             }
         }
-        private void check(object sender, EventArgs e)
+        private void check(object sender, EventArgs e) // Funktion, die neues picture in Box einliest, solang im Orner neue vorhanden sind und gibt alte frei
         {
-            if (i < files.Count())
+            if (i < files.Count()) //hochzählen bis maximale Anzahl Files
             {
-                if (pictureBox1.Image != null)
+                if (pictureBox1.Image != null)  //wenn Image geladen ist
                 {
 
                 }
-                TextBox.Text = files[i];
-                if (currentPic != null)
+                TextBox.Text = files[i];  // file in entsprechender TextBox
+                if (currentPic != null)  // wenn picture vorhanden 
                 {
-                    currentPic.Dispose();
+                    currentPic.Dispose();  // picture/aktuelle Bitmapinstanz freigeben
                 }
-                currentPic = new Bitmap(files[i]);
-                pictureBox1.Image = currentPic;
-                i++;
+                currentPic = new Bitmap(files[i]); //aktuelles file als neue Bitmapinstnaz/aktuelles picture
+                pictureBox1.Image = currentPic; //neue Instanz/aktuelles picture in Box laden
+                i++; //hochzählen der Instanzen/files und somit pictures/Images
             }
             else
-                timer.Stop();
+                timer.Stop(); //Ende timer wenn letzte erreicht
 
         }
 
